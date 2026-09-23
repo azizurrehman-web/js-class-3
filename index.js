a5463422c02527ba642b8d8b8c6fc770ff7464bb
Aziz ur Rehman <azizurrehmanweb0@gmail.com>
2026-09-23 18:16:22 +0500

    goods update

---
+++ index.js
@@ -0,0 +1,28 @@
+goods = prompt("enter your goods");
+if (goods == "bottle") {
+  alert("bottle is available");
+} else if (goods == "glass") {
+  console.log("glass hamara pass nahi ha");
+} else if (goods == "remote") {
+  console.log("remote is not available");
+} else {
+  console.log("wrong entry");
+}
+
+
+a = +prompt("enter your 1st number");
+b = +prompt("enter your 2nd number");
+c = a + b;
+console.log(c);
+
+let d = "Hasib";
+d = "Ahmad";
+d = 22;
+d;
+console.log(d);
+
+const e = 44;
+e = "Ahmed";
+e = true;
+e;
+console.log(e);
